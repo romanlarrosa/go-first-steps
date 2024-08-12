aa8cc3189704f1e8cf354f385d25bbf82deb75ac
Román Larrosa Lewandowska <rlarrosa@certinia.com>
2024-08-12 19:40:43 +0200

    Add randomFormat function

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 /*
@@ -13,6 +14,21 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+/*
+RandomFormat return one of a set of greeting messages.
+The returned message is selected randomly
+*/
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Returns a random format, usign rand module Intn function.
+	return formats[rand.Intn(len(formats))]
 }
