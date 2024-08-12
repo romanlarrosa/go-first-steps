11fc4cd6289d23500870fbbaf78c4b21261df6d8
Román Larrosa Lewandowska <rlarrosa@certinia.com>
2024-08-12 19:26:00 +0200

    Handle errors in hello module

--- hello/hello.go
+++ hello/hello.go
@@ -6,9 +6,24 @@ import (
 	"rsc.io/quote"
 
 	"example.com/greetings"
+
+	"log"
 )
 
 func main() {
-	fmt.Println(greetings.Hello("John"))
+	//Set the log entry prefix
+	log.SetPrefix("greetings: ")
+	//This flag disable printing tume, source file and line number
+	log.SetFlags(0)
+
+	//Request a greeting message
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
+	// Get a quote
 	fmt.Println(quote.Go())
 }
