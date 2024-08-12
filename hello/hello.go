f3b003a28aed31b11684adf1b111d49107ae5380
Román Larrosa Lewandowska <rlarrosa@certinia.com>
2024-08-12 20:15:49 +0200

    Get only one name

--- hello/hello.go
+++ hello/hello.go
@@ -17,10 +17,10 @@ func main() {
 	log.SetFlags(0)
 
 	// An array of names
-	names := []string{"John", "Deborah", "Gladys"}
+	names := []string{"Felix", "Deborah", "Gladys"}
 
 	//Request a greeting message
-	messages, err := greetings.Hellos(names)
+	messages, err := greetings.Hello(names[0])
 
 	if err != nil {
 		log.Fatal(err)
