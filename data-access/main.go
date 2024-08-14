bc5c7ac885c642385701f685fe02e060b3e2b42f
Román Larrosa Lewandowska <rlarrosa@certinia.com>
2024-08-14 09:57:49 +0200

    connect to the database

--- data-access/main.go
+++ data-access/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+var db *sql.DB
+
 func main() {
-	
+	// Load env vars
+	godotenv.Load("./data/.env")
+
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:   os.Getenv("MYSQLUSER"),
+		Passwd: os.Getenv("MYSQLPASSWORD"),
+		Net:    "tcp",
+		Addr:   os.Getenv("MYSQL_PUBLIC_URL"),
+		DBName: os.Getenv("MYSQL_DATABASE"),
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
