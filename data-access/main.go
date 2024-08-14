ff30325f881b01752adf59cadb766a5f654ba509
Román Larrosa Lewandowska <rlarrosa@certinia.com>
2024-08-14 12:57:21 +0200

    Add an album

--- data-access/main.go
+++ data-access/main.go
@@ -46,6 +46,7 @@ func main() {
 	}
 	fmt.Println("Connected!")
 
+	// Get albums by artist
 	artist := "John Coltrane"
 	fmt.Printf("Querying albums by artist: %s...\n", artist)
 	albums, err := albumsByArtist(artist)
@@ -62,6 +63,7 @@ func main() {
 	}
 	w.Flush()
 
+	// Get album by id
 	albumId := 3
 	fmt.Printf("Querying album by ID: %d...\n", albumId)
 	alb, err := albumByID(2)
@@ -73,6 +75,17 @@ func main() {
 	fmt.Fprintln(w, "--\t-----\t------\t-----")
 	fmt.Fprintf(w, "%d\t%s\t%s\t%f\n", alb.ID, alb.Title, alb.Artist, alb.Price)
 	w.Flush()
+
+	//Add an album
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
 }
 
 // albumsByArtist queries for albums that have the specified artist name.
@@ -113,4 +126,18 @@ func albumByID(id int64) (Album, error) {
 		return alb, fmt.Errorf("albumsById %d: %v", id, err)
 	}
 	return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
 }
